ae63f28af64f1822240f7ee6f8a761b6453f7815
Wikator <wiktor@szymulewicz.com>
2023-10-30 18:17:50 +0100

    Made description not required

---
+++ ToDoList/ToDoList.DataAccess/Migrations/20231030171315_MadeDescriptionNullable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class MadeDescriptionNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Colleges",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Colleges",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
--- ToDoList/ToDoList.Models/College.cs
+++ ToDoList/ToDoList.Models/College.cs
@@ -15,7 +15,6 @@ namespace ToDoList.Models
         [StringLength(100)]
         public string Title { get; set; }
 
-        [Required]
         [StringLength(500)]
         public string Description { get; set; }
 
